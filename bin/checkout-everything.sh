22d3bd97fb1ef4cc7f32655db3b967ed510e5439
Hannah <xhan104@gmail.com>
2017-06-05 14:47:05 +0100

    NLW-298

---
+++ bin/checkout-everything.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git clone git@github.com:digirati-co-uk/nlw-frontend.git 
+git clone git@github.com:digirati-co-uk/ida-frontend.git
+cd omeka-workspace/modules
+git clone git@github.com:digirati-co-uk/omeka-elucidate-module.git Elucidate
+git clone git@github.com:digirati-co-uk/omeka-public-user-module.git Kindred
+git clone git@github.com:digirati-co-uk/omeka-resource-provider.git ResourceProvider
+git clone git@github.com:digirati-co-uk/omeka-iiif-view-module.git IIIFView
+git clone git@github.com:digirati-co-uk/omeka-iiif-import-module.git IIIFImport
+git clone git@github.com:digirati-co-uk/omeka-annotation-studio-module.git AnnotationStudio
+git clone git@github.com:digirati-co-uk/OAuth.git OAuth
+
